4d6606116177d42673c839cbd7ae52ee9cb2d2a3
zcaalock <zcaalock@yahoo.com>
2017-06-19 23:39:37 +0200

    create store

--- src/app.js
+++ src/app.js
@@ -4,5 +4,10 @@ import {createStore} from 'redux'
 //Step 3 define reducers
 
 //Step 1 create the store
+const store = createStore(reducers)
+
+store.subscribe( () => {
+    console.log('current state is: ' + store.getState())
+})
 
 //Step 2 create and dispatch actions
