7e95c1020c5650f757d110906732683bede54bc9
zcaalock <zcaalock@yahoo.com>
2017-06-19 23:50:48 +0200

    increment decrement actions

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@
 import {createStore} from 'redux'
 
 const INCREMENT = 'increment'
+const DECREMENT = 'decrement'
 
 //Step 3 define reducers
 const reducer = function(state=0, action){
     switch(action.type){
         case INCREMENT:
         return state + action.payload;
+        case DECREMENT:
+        return state - action.payload;
     }
     return state
 }
 
 //Step 1 create the store
-const store = createStore(reducers)
+const store = createStore(reducer)
 
 store.subscribe(function(){
     console.log('current state is: ' + store.getState())
@@ -23,6 +26,13 @@ store.subscribe(function(){
 store.dispatch(
     {
         type: INCREMENT,
+        payload: 1
+    }
+)
+
+store.dispatch(
+    {
+        type: DECREMENT,
         payload: 1
     }
 )
